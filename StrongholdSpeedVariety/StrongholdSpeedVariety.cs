51da9bb2b45755101bd16a1e6db78cd8f4cc66ed
Wilps <demi@wilps.ru>
2024-07-20 00:59:08 +0700

    Update StrongholdSpeedVariety.cs

--- StrongholdSpeedVariety/StrongholdSpeedVariety.cs
+++ StrongholdSpeedVariety/StrongholdSpeedVariety.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
@@ -29,7 +28,7 @@ namespace StrongholdSpeedVariety
             {
                 if (!found90 && instruction.opcode == OpCodes.Ldc_I4_S && (sbyte)instruction.operand == 90)
                 {
-                    list.Add(new CodeInstruction(OpCodes.Ldc_I4, 1000)); // Изменяем тип операнда на int
+                    list.Add(new CodeInstruction(OpCodes.Ldc_I4, 1000)); // Change operand type to int
                     found90 = true;
                     MelonLogger.Msg("IncreaseFrameRate patched!");
                 }
@@ -40,20 +39,19 @@ namespace StrongholdSpeedVariety
             }
             if (!found90)
                 throw new ArgumentException("Cannot find upper limit in Director.IncreaseFrameRate");
-            return list;
+            return InjectStep(list);
         }
 
-        [HarmonyTranspiler]
-        public static IEnumerable<CodeInstruction> InjectStep(IEnumerable<CodeInstruction> instructions)
+        private static IEnumerable<CodeInstruction> InjectStep(IEnumerable<CodeInstruction> instructions)
         {
-            var found5 = false;
             var list = new List<CodeInstruction>();
             foreach (var instruction in instructions)
             {
-                if (!found5 && instruction.opcode == OpCodes.Ldc_R8 && (double)instruction.operand == 5.0)
+                if (instruction.opcode == OpCodes.Ldc_R8 && (double)instruction.operand == 5.0)
                 {
-                    list.Add(new CodeInstruction(OpCodes.Ldc_R8, 5.0));
-                    found5 = true;
+                    // Replace with logic to choose step based on current frame rate
+                    list.Add(new CodeInstruction(OpCodes.Ldarg_0)); // Load `this` (Director instance)
+                    list.Add(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(GameSpeedPatchIncrease), nameof(GetStep))));
                 }
                 else
                 {
@@ -61,6 +59,18 @@ namespace StrongholdSpeedVariety
                 }
             }
             return list;
+        }
+
+        public static double GetStep(Director director)
+        {
+            double currentFrameRate = director.EngineFrameRate;
+            if (currentFrameRate >= 10 && currentFrameRate < 90)
+                return 5.0;
+            if (currentFrameRate >= 90 && currentFrameRate < 200)
+                return 10.0;
+            if (currentFrameRate >= 200 && currentFrameRate <= 1000)
+                return 100.0;
+            return 5.0; // default step
         }
     }
 
@@ -77,7 +87,8 @@ namespace StrongholdSpeedVariety
             {
                 if (!found5 && instruction.opcode == OpCodes.Ldc_R8 && (double)instruction.operand == 5.0)
                 {
-                    list.Add(new CodeInstruction(OpCodes.Ldc_R8, 5.0));
+                    list.Add(new CodeInstruction(OpCodes.Ldarg_0)); // Load `this` (Director instance)
+                    list.Add(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(GameSpeedPatchDecrease), nameof(GetStep))));
                     found5 = true;
                     MelonLogger.Msg("DecreaseFrameRate patched!");
                 }
@@ -89,14 +100,26 @@ namespace StrongholdSpeedVariety
             if (!found5)
                 throw new ArgumentException("Cannot find upper limit in Director.DecreaseFrameRate");
             return list;
-        }   
+        }
+
+        public static double GetStep(Director director)
+        {
+            double currentFrameRate = director.EngineFrameRate;
+            if (currentFrameRate >= 10 && currentFrameRate <= 90)
+                return 5.0;
+            if (currentFrameRate > 90 && currentFrameRate <= 200)
+                return 10.0;
+            if (currentFrameRate > 200 && currentFrameRate <= 1000)
+                return 100.0;
+            return 5.0; // default step
+        }
     }
 
     [HarmonyPatch(typeof(Director))]
     [HarmonyPatch("SetEngineFrameRate")]
     public static class GameSpeedPatchSet
     {
-        // Новый метод для вставки в IL-код
+        // New method for inserting IL code
         private static IEnumerable<CodeInstruction> InjectEngineFrameRateLog(IEnumerable<CodeInstruction> instructions)
         {
             var list = new List<CodeInstruction>();
@@ -104,16 +127,16 @@ namespace StrongholdSpeedVariety
             {
                 list.Add(instruction);
 
-                // Вставляем инструкцию для вывода в консоль после сохранения engineFrameTime
+                // Insert instruction to output to console after saving engineFrameTime
                 if (instruction.opcode == OpCodes.Stfld && ((FieldInfo)instruction.operand).Name == "engineFrameTime")
                 {
-                    // Загружаем ссылку на текущий объект Director
+                    // Load reference to the current Director object
                     list.Add(new CodeInstruction(OpCodes.Ldarg_0));
-                    // Вызываем геттер EngineFrameRate
+                    // Call EngineFrameRate getter
                     list.Add(new CodeInstruction(OpCodes.Callvirt, AccessTools.Property(typeof(Director), "EngineFrameRate").GetGetMethod()));
-                    // Преобразуем в строку
+                    // Convert to string
                     list.Add(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Convert), "ToString", new Type[] { typeof(int) })));
-                    // Вызываем MelonLogger.Msg для вывода в консоль
+                    // Call MelonLogger.Msg to output to console
                     list.Add(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(MelonLogger), "Msg", new Type[] { typeof(string) })));
                 }
             }
@@ -141,7 +164,7 @@ namespace StrongholdSpeedVariety
             if (!found)
                 throw new ArgumentException("Cannot find upper limit in Director.SetEngineFrameRate");
 
-            // Вставляем код для вывода engineFrameTime в консоль
+            // Insert code to output engineFrameTime to console
             return InjectEngineFrameRateLog(list);
         }
     }
